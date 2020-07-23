e85386832c2bd71b757aa819cd7704d8a66e7c83
Shayla Luong <nganluong1021@gmail.com>
2020-07-22 21:04:35 -0700

    initial set-up of main page structure

---
+++ src/assets/components/Content.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+// right side column containing all sections
---
+++ src/assets/components/SideBar.js
@@ -0,0 +1,6 @@
+
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+
+// left column of Home page, containing basic Profile, Navigation, and Contacts
