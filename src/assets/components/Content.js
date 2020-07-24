d940da2044355b964747a9448bbb57bd7bc1c2b2
Shayla Luong <nganluong1021@gmail.com>
2020-07-23 22:39:23 -0700

    implmeneted basic side bar

--- src/assets/components/Content.js
+++ src/assets/components/Content.js
@@ -3,9 +3,34 @@ import Container from 'react-bootstrap/Container';
 
 // right side column containing all sections
 class Content extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            experience: [
+                {
+                    id: 0,
+                    jobTitle: '',
+                    description: '',
+                    selected: false
+                }
+            ],
+            education: [
+                {
+                    id: 0,
+                    school: 'University of California, Irvine',
+                    description: '',
+                    selected: false
+                }
+            ]
+        }
+    }
+
     render() {
         return (
-            <p>Content</p>
+            <div style={{marginLeft:250}}>
+                <p>Content</p>
+            </div>
         );
     }
 }
--- src/assets/components/SideBar.js
+++ src/assets/components/SideBar.js
@@ -5,7 +5,20 @@ import Container from 'react-bootstrap/Container';
 class SideBar extends React.Component {
     render() {
         return (
-            <p>SideBar</p>
+            <nav id="sidebar">
+                <Container className="justify-content-center py-5"><h3>Shayla Luong</h3></Container>
+                <ul className="nav flex-item">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">About Me</a></li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#education">Education</a></li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#experience">Experience</a></li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="Projects">Projects</a></li>
+                </ul>
+            </nav>
+
         );
     }
 }
