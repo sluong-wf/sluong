72a16f685076b110804840213f778e6512337e8f
Shayla Luong <nganluong1021@gmail.com>
2020-07-24 20:31:19 -0700

    moved components directory out of assets

---
+++ src/components/Social.js
@@ -0,0 +1,17 @@
+import github from '../assets/images/github.svg'
+import linkedin from '../assets/images/linkedin.svg'
+
+export default [
+    {
+        id: 3,
+        title: "Github",
+        icon: github,
+        path: "https://github.com/nluong1021"
+    },
+    {
+        id: 4,
+        title: "LinkedIn",
+        icon: linkedin,
+        path: "https://www.linkedin.com/in/shayla-luong"
+    },
+];
