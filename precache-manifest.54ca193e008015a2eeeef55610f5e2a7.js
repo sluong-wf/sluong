9f5cae68002b5f2e0ea527e26bb1b2016d7b7104
Shayla Luong <shayla.luong@sjsu.edu>
2021-09-22 10:40:54 -0700

    Updates

---
+++ precache-manifest.54ca193e008015a2eeeef55610f5e2a7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cac43a91043c8b69f60ee998ec619bd",
+    "url": "/sluong/index.html"
+  },
+  {
+    "revision": "05760dd7a40c73b95260",
+    "url": "/sluong/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "516766215fe3612efd40",
+    "url": "/sluong/static/css/main.6b63bc0e.chunk.css"
+  },
+  {
+    "revision": "05760dd7a40c73b95260",
+    "url": "/sluong/static/js/2.8121d863.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sluong/static/js/2.8121d863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "516766215fe3612efd40",
+    "url": "/sluong/static/js/main.3208cad6.chunk.js"
+  },
+  {
+    "revision": "7b94c7697cb466955128",
+    "url": "/sluong/static/js/runtime-main.f99ec774.js"
+  },
+  {
+    "revision": "652c9ad6a3d0c704508e02a2283b122f",
+    "url": "/sluong/static/media/profile_pic.652c9ad6.jpg"
+  },
+  {
+    "revision": "c0459bdec10d6e0225598e6b7537d9ae",
+    "url": "/sluong/static/media/project-blink.c0459bde.jpg"
+  },
+  {
+    "revision": "4b00d47f17b944b1e9c2bb7ac60020f9",
+    "url": "/sluong/static/media/project-chickinfinite.4b00d47f.jpg"
+  },
+  {
+    "revision": "4ea96ad021166d53137423c39ffbc5e2",
+    "url": "/sluong/static/media/project-dare.4ea96ad0.jpg"
+  },
+  {
+    "revision": "25d76538a302af0484900e89448979d4",
+    "url": "/sluong/static/media/project-inbetween.25d76538.jpg"
+  },
+  {
+    "revision": "f6977ce8832666ce862e61711d4e3227",
+    "url": "/sluong/static/media/project-smartbins.f6977ce8.jpg"
+  },
+  {
+    "revision": "ebeb8ae66f6a4ed5157ca648c4491b69",
+    "url": "/sluong/static/media/resume.ebeb8ae6.png"
+  },
+  {
+    "revision": "850abb3131c40e386ecb6f5e615ab58e",
+    "url": "/sluong/static/media/sluong-resume.850abb31.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2d47fcb3eeaf73492f2b25a9104c10e.js"
+  "/sluong/precache-manifest.54ca193e008015a2eeeef55610f5e2a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sluong/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
